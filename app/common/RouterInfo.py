6625d0ba6c9a1fb7983fa5ae88fe82a91a2956d8
Ladbaby <1390265970@qq.com>
2023-08-07 19:16:29 +0800

    fix: timeout set to 100ms

--- app/common/RouterInfo.py
+++ app/common/RouterInfo.py
@@ -82,7 +82,7 @@ class Router_HW:
 
     def get_session_token(self):
         try:
-            response_session = requests.get(self.session_url, headers=self.headers, timeout=0.5, verify=False)
+            response_session = requests.get(self.session_url, headers=self.headers, timeout=0.1, verify=False)
 
             if response_session.status_code == 200:
                 # print(response.text)
@@ -126,7 +126,7 @@ class Router_HW:
     def get_status(self):
         self.get_session_token()
         try:
-            response_status = requests.get(self.status_url, headers=self.headers, timeout=0.5, verify=False, cookies=self.cookies)
+            response_status = requests.get(self.status_url, headers=self.headers, timeout=0.1, verify=False, cookies=self.cookies)
             if response_status.status_code == 200:
                 with concurrent.futures.ThreadPoolExecutor(max_workers=1) as executor:
                     executor.submit(self.write_debug_xml, response_status, "status")
@@ -303,7 +303,7 @@ class Router_HW:
     def get_traffic_statistics(self):
         self.get_session_token()
         try:
-            response_status = requests.get(self.traffic_statistics_url, headers=self.headers, timeout=0.5, verify=False, cookies=self.cookies)
+            response_status = requests.get(self.traffic_statistics_url, headers=self.headers, timeout=0.1, verify=False, cookies=self.cookies)
             if response_status.status_code == 200:
                 with concurrent.futures.ThreadPoolExecutor(max_workers=1) as executor:
                     executor.submit(self.write_debug_xml, response_status, "traffic_statistics")
@@ -346,7 +346,7 @@ class Router_HW:
         self.get_session_token()
         try:
             response_status = requests.get(
-                self.month_statistics_url, headers=self.headers, timeout=0.5, verify=False, cookies=self.cookies
+                self.month_statistics_url, headers=self.headers, timeout=0.1, verify=False, cookies=self.cookies
             )
             if response_status.status_code == 200:
                 with concurrent.futures.ThreadPoolExecutor(max_workers=1) as executor:
@@ -391,7 +391,7 @@ class Router_HW:
         self.get_session_token()
         try:
             response_status = requests.get(
-                self.start_date_url, headers=self.headers, timeout=0.5, verify=False, cookies=self.cookies
+                self.start_date_url, headers=self.headers, timeout=0.1, verify=False, cookies=self.cookies
             )
             if response_status.status_code == 200:
                 with concurrent.futures.ThreadPoolExecutor(max_workers=1) as executor:
